91c863174dbbb477b77e4629743013c4280b578a
Andrew on Mac <mj.apply.sci@gmail.com>
2024-05-12 23:01:26 +0100

    starting on mac configurations

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,9 +1,24 @@
 import Cocoa
 import FlutterMacOS
+import UserNotifications
+import alarm
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
+  }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+      if #available(iOS 10.0, *) {
+          UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+      }
+      SwiftAlarmPlugin.registerBackgroundTasks()
+      
+      GeneratePluginRegistrant.register(with: self)
+      return super.application(application, didFinishLaunchingWithOptions:
+                                launchOptions)
   }
 }
