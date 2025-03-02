be27f35731f673eed3445281e38b80c49b7e2871
AZPS <33700551+ApplySci@users.noreply.github.com>
2025-03-02 13:04:01 +0000

    drop that dialog, and just force a restart on migration if we need one

--- lib/providers/migration.dart
+++ lib/providers/migration.dart
@@ -15,34 +15,10 @@ class MigrationService {
   MigrationService(this.ref);
 
   Future<void> checkMigration(BuildContext context) async {
-    final storedVersion = ref.read(appVersionProvider);
-    Log.debug('Checking migration. Stored version: $storedVersion');
+    final storedVersion = ref.read(appVersionProvider);Log.debug('***** Stored version: $storedVersion');
 
     // If we have legacy prefs, but no version number, reset FCM notifications
     if (storedVersion == "0.0.0") {
-      await showDialog<bool>(
-        context: context,
-        barrierDismissible: false,
-        builder: (dialogContext) => MaterialApp(
-          home: Builder(
-            builder: (innerContext) => AlertDialog(
-              title: const Text('App Update'),
-              content: const Text(
-                'The app needs to reset notification settings. '
-                'We need to restart.'
-              ),
-              actions: [
-                FilledButton(
-                  onPressed: () => Navigator.pop(dialogContext, true),
-                  child: const Text('OK'),
-                ),
-              ],
-            ),
-          ),
-          debugShowCheckedModeBanner: false,
-        ),
-      );
-
       // Update stored version
       await ref.read(appVersionProvider.notifier).set(currentAppVersion);
       final service = ref.read(notificationPreferencesProvider);
